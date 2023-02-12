6f207326513f1870a189e190c9040d1d661075be
DevR <azammmgol@gmail.com>
2023-02-12 16:17:06 +0600

    fix: put empty string instead of null in seo title

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -9,7 +9,7 @@ export function SEO(props: SeoProps) {
   const { title, description } = props;
   return (
     <Head>
-      <title>{`LinkVault${title ? ` / ${title}` : null}`}</title>
+      <title>{`LinkVault${title ? ` / ${title}` : ""}`}</title>
       {description && <meta name="description" content={description} />}
     </Head>
   );
