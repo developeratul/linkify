78024b21e64c2139f476354c76a5e847326d6d81
DevR <azammmgol@gmail.com>
2023-02-17 18:29:33 +0600

    updated the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,4 +8,18 @@ export const theme = extendTheme({
     cursive: "Caveat",
     fantasy: "fantasy",
   },
+  colors: {
+    gray: {
+      50: "#F9FAFB",
+      100: "#EFF2F6",
+      200: "#DFE4EC",
+      300: "#CFD7E3",
+      400: "#C2CCDB",
+      500: "#B1BED1",
+      600: "#8296B5",
+      700: "#556D90",
+      800: "#394960",
+      900: "#1C2430",
+    },
+  },
 });
